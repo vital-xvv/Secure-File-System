d5ee2cf6c3052de107e4e82076c3a21ef5b5da48
vital-xvv <vital.xvv@gmail.com>
2024-04-14 14:48:14 +0300

    Created some console interface params validation.

--- src/main/java/io/vital/app/Main.java
+++ src/main/java/io/vital/app/Main.java
@@ -14,27 +14,48 @@ public class Main {
         String folderPath = args[0];
         String value = args[1];
         String xmlPath = args[2];
-        int threadNum = Integer.parseInt(args[3]);
+        int threadNum = 2;
 
-        //Dev app testing
-//        String folderPath = "src/main/resources/json_files_for_experiment";
-//        String value = "language";
-//        String xmlPath = "src/main/resources/statistics_xml";
-//        int threadNum = 8;
+        //Params validation
+        if(args.length == 4 && args[3] != null) {
+            try {
+                threadNum = Integer.parseInt(args[3]);
+            }catch (NumberFormatException e) {
+                System.err.printf("\n\nIllegal whole number literal. Default number of threads - %d.\n", threadNum);
+            }
+        }
 
+        if(threadNum <= 0){
+            System.err.println("\nIllegal whole number literal (args[3]). threadNum should be greater than 0.\n");
+            return;
+        }
+
+        if(!new java.io.File(folderPath).exists() || !new java.io.File(folderPath).isDirectory()){
+            System.err.println("\nIllegal argument for folderPath (args[0]): the folder does not exist or it's a file.\n");
+            return;
+        }
+
+        if(!new java.io.File(xmlPath).exists() || !new java.io.File(xmlPath).isDirectory()){
+            System.err.println("\nIllegal argument for xmlPath (args[2]): the folder does not exist or it's a file\n");
+            return;
+        }
+
+        //Constructing full xml folder path
         String statisticsFilePath = Path.of(String.format("%s/statistics_by_%s.xml",xmlPath, value)).toString();
 
+        //Parsing and calculating exec time
         long start = System.currentTimeMillis();
         Statistics statistics = jsonParser.parseFilesInFolderEfficientConcurrent(folderPath, value, threadNum);
         long end = System.currentTimeMillis();
 
         statistics.sorted();
 
+        //Writing serialized xml statistics into file
         jsonParser.prettyXmlPrintInFile(xmlPath, statistics, value);
 
         //Pretty result print in console
         System.out.printf("\n%d threads. Parsed folder %s for: %.3f sec\n", threadNum, folderPath, (end-start)/1000.0);
         System.out.println("\n- - - - - - - - - - - - - - - - -\n");
-        System.out.printf("File %s has been created.%n", statisticsFilePath);
+        System.out.printf("File %s has been created.%n\n", statisticsFilePath);
     }
 }
